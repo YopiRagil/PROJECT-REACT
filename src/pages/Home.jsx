f97a6a4786952e9ddd687dfb5059cf7539e98d6d
Fazlur Rahman <fazlur@alterra.id>
2020-05-07 14:47:22 +0700

    sidebar

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,14 +3,17 @@ import { withRouter } from "react-router-dom";
 import { Redirect } from "react-router-dom";
 import { connect } from "react-redux";
 // import { logoutUser } from "../store/actions/userAction";
-import Header from "../component/Header"
-
+import Header from "../component/Header";
+import Sidebar from "../component/Sidebar";
 class Home extends Component {
-    render() {
-        return (
-            <Header />
-        )
-    }
+  render() {
+    return (
+      <React.Fragment>
+        <Header />
+        <Sidebar />
+      </React.Fragment>
+    );
+  }
 }
 
 export default withRouter(Home);
