b471942842ffda36eddb67e64660dc0cbb6192ea
anovado <novado@alterra.id>
2020-05-07 14:34:58 +0700

    adding banner and productresult component

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,11 +4,22 @@ import { Redirect } from "react-router-dom";
 import { connect } from "react-redux";
 // import { logoutUser } from "../store/actions/userAction";
 import Header from "../component/Header"
+import Banner from "../component/Banner"
+import ProductResult from "../component/ProductResult"
 
 class Home extends Component {
     render() {
         return (
-            <Header />
+            <div>
+                <Header />
+                <div className="text-center mt-4" id="recommendation-title">
+                    <h1>OUR RECOMMENDATIONS</h1>
+                </div>
+                <Banner />
+                <ProductResult />
+                <ProductResult />
+                <ProductResult />
+            </div>
         )
     }
 }
