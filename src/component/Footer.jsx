745c6342e20ec705aeea74ec60ea0a73a24d6e85
anovado <novado@alterra.id>
2020-05-08 04:04:36 +0700

    adding footer, fixing font imports

---
+++ src/component/Footer.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+const Footer = (props) => {
+  return (
+
+    <footer id="sticky-footer" className="py-4 text-dark-50">
+      <div class="container text-center">
+        <small><strong>Copyright &copy; Mlaku-Mlaku 2020</strong></small>
+      </div>
+    </footer>
+  )
+};
+
+export default Footer;
--- src/component/Header.jsx
+++ src/component/Header.jsx
@@ -6,35 +6,36 @@ import ToolProfil from "./ToolProfil";
 class Header extends Component {
   render() {
     return (
-      <nav className="navbar navbar-expand-lg navbar-light">
-        {/* <div className="container"> */}
-        <a className="navbar-brand ml-lg-5" href="#">
-          <img
-            className="navbar-brand"
-            src={require("../assets/images/mlaku.png")}
-            class="navbar-brand"
-            alt="logo"
-            style={{ width: "15vmin" }}
-          />
-        </a>
-        <button
-          className="navbar-toggler"
-          type="button"
-          data-toggle="collapse"
-          data-target="#navbarSupportedContent"
-          aria-controls="navbarSupportedContent"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
-        >
-          <span className="navbar-toggler-icon"></span>
-        </button>
-        <div className="collapse navbar-collapse" id="navbarSupportedContent">
-          <Search {...this.props} />
-          <div className="ml-lg-auto mr-lg-5">
-            <ToolProfil {...this.props} />
-          </div>
+      <nav className="navbar navbar-expand-lg navbar-light row">
+
+        <div className="col-sm-1 mr-5">
+          <a className="navbar-brand ml-lg-5" href="#">
+            <img
+              className="navbar-brand"
+              src={require("../assets/images/mlaku.png")}
+              class="navbar-brand"
+              alt="logo"
+              style={{ width: "15vmin" }}
+            />
+          </a>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-toggle="collapse"
+            data-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
         </div>
-        {/* </div> */}
+
+        <Search {...this.props} />
+        <div className="ml-lg-auto mr-lg-5 col-sm-2">
+          <ToolProfil {...this.props} />
+        </div>
+
       </nav>
     );
   }
